6206e8fd999292c8e151816cdfd9e0eefb9e12e9
Dan Cahoon <dpcahoon@gmail.com>
2021-12-01 22:53:42 -0500

    Clean up day 1 solution

--- src/Day01.hs
+++ src/Day01.hs
@@ -5,22 +5,23 @@ import Data.List
 import Util (Solution)
 
 part1 :: [Int] -> Int -> Int
-part1 (x:y:ys) num
-  | y > x = part1 (y:ys) (num + 1)
-  | otherwise = part1 (y:ys) num
-part1 _ num = num
+part1 (x:y:ys) count
+  | y > x = part1 (y:ys) (count + 1)
+  | otherwise = part1 (y:ys) count
+part1 _ count = count
 
-part2 :: [Int] -> Int -> Int -> Int
-part2 (x:y:z:zs) prev num
-  | window > prev = part2 (y:z:zs) window (num + 1)
-  | otherwise = part2 (y:z:zs) window num
-  where window = x + y + z
-part2 _ _ num = num
+part2 :: [Int] -> Int -> Int
+part2 (w:x:y:z:zs) count
+  | windowB > windowA = part2 (x:y:z:zs) count + 1
+  | otherwise         = part2 (x:y:z:zs) count
+  where windowA = w + x + y
+        windowB = x + y + z
+part2 _ count = count
 
 solution :: Solution
 solution = do
     contents <- readFile "src/inputs/day01.txt"
     let lst = map read (words contents) :: [Int]
     let answer1 = show $ part1 lst 0
-    let answer2 = show $ part2 lst 10000 0
+    let answer2 = show $ part2 lst 0
     return (answer1, answer2)
