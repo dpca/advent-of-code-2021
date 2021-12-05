b428e6689c27f37f9a41bbe06ab496b58a7192b0
Dan Cahoon <dpcahoon@gmail.com>
2021-12-05 01:54:21 -0500

    Clean up tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,18 +8,20 @@ import qualified Day05
 
 main :: IO ()
 main = hspec $ do
-    describe "Solutions" $ do
-        it "Day 1 returns the correct answers" $ do
+    describe "Day 1" $ do
+        it "correctly solves the puzzle" $ do
             (part1, part2) <- Day01.solution
             part1 `shouldBe` "1477"
             part2 `shouldBe` "1523"
 
-        it "Day 2 returns the correct answers" $ do
+    describe "Day 2" $ do
+        it "correctly solves the puzzle" $ do
             (part1, part2) <- Day02.solution
             part1 `shouldBe` "2027977"
             part2 `shouldBe` "1903644897"
 
-        it "Day 3 returns the correct answers" $ do
+    describe "Day 3" $ do
+        it "correctly solves the puzzle" $ do
             (part1, part2) <- Day03.solution
             part1 `shouldBe` "749376"
             part2 `shouldBe` "2372923"
@@ -30,7 +32,7 @@ main = hspec $ do
             part1 `shouldBe` "4512"
             part2 `shouldBe` "1924"
 
-        it "correctly solves the full puzzle" $ do
+        it "correctly solves the puzzle" $ do
             (part1, part2) <- Day04.solution
             part1 `shouldBe` "16716"
             part2 `shouldBe` "4880"
@@ -41,7 +43,7 @@ main = hspec $ do
             part1 `shouldBe` "5"
             part2 `shouldBe` "12"
 
-        it "correctly solves the full puzzle" $ do
+        it "correctly solves the puzzle" $ do
             (part1, part2) <- Day05.solution
             part1 `shouldBe` "4745"
             part2 `shouldBe` "18442"
